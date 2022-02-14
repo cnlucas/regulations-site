1abb8e20e2aae3a7e56a4a3500fb187cc0e441f8
Priyanka Kasireddy <pkasireddy@fec.gov>
2022-02-14 15:57:11 -0500

    upgrade django to v3.2.12

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'cached-property',
-        'django==3.2.10',
+        'django==3.2.12',
         'enum34;python_version<"3.4"',
         'futures;python_version<"3"',
         'requests',
